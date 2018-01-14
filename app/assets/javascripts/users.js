e336d7bdc09ae71fa2e577e4ac19eec8b8c7a490
Agnieszka Domanska <domanska.a.m.87@gmail.com>
2018-01-14 20:30:13 +0000

    Stripe response handler and error handling js added

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -8,19 +8,54 @@ $(document).on('turbolinks:load',function(){
   btnSubmit.click(function(event){
     //prevent default submission behavior.
     event.preventDefault();
+    btnSubmit.val("Processing...").prop('disabled',true);
     //Collect the credit card fields.
     var ccNum = $('#card_number').val(),
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-    //Send the card info to Stripe.
-    Stripe.createToken({
-      number: ccNum,
-      cvc: cvcNum,
-      exp_month: expMonth,
-      exp_year: expYear
-    }, stripeResponseHandler);
+        
+    var error = false;
+    
+    if(!Stripe.card.validateCardNumber(ccNum)){
+      error = true;
+      alert('Invalid card number');
+    }
+    
+    if(!Stripe.card.validateCVC(cvcNum)){
+      error = true;
+      alert('Invalid cvc number');
+    }
+    
+    if(!Stripe.card.validateExpiry(expMonth, expYear)){
+      error = true;
+      alert('Invalid expiration date');
+    }
+        
+    if (error){
+      btnSubmit.val("Sign up").prop('disabled', false);
+    } else {
+      //Send the card info to Stripe.
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    
+    return false;
   });
+  
+  //Stripe will return a card token.
+  function stripeResponseHandler(status,response){
+    //Get the token from the response.
+    var token = response.id;
+    //Inject the card token in a hidden field.
+    $('#theForm').append($('<input_type="hidden" name="user[stripe_card_token]">').val(token));
+    //Submit form to our Rails app.
+    $('#theForm').get(0).submit();
+  }
     
     
   //Stripe will return a card token.
